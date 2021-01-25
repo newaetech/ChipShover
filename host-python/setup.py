e2250d43e834871320caa25bf62dca2c700abe2c
Colin O'Flynn <coflynn@newae.com>
2021-01-25 15:43:56 -0400

    Update pypi to have URLs etc

--- host-python/setup.py
+++ host-python/setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='chipshover',
     version='0.2.0',
-    url='https://github.com/newaetech/ChipSHOVER',
+    url='https://github.com/newaetech/ChipShover',
     author='NewAE Technology Inc.',
     author_email='coflynn@newae.com',
-    description='XY(Z) Controller Interface for ChipShover.',
+    description='XYZ Controller Interface for ChipShover.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     packages=find_packages(),
     install_requires=['pyserial >= 3.4'],
+    project_urls={
+        'Documentation': 'https://chipshover.readthedocs.io',
+        'Source': 'https://github.com/newaetech/chipshover',
+        'Issue Tracker': 'https://github.com/newaetech/chipshover/issues',
+    },
 )
